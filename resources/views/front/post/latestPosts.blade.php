6941277c1d10b5355d7ad79d96e9b99b96ef56d3
DaffaPrk <Daffa123_>
2022-10-20 14:19:05 +0700

    a

--- resources/views/front/post/latestPosts.blade.php
+++ resources/views/front/post/latestPosts.blade.php
@@ -151,7 +151,7 @@
                       <h1 class="mx-4 my-4 text-center md:text-3xl font-extrabold">
                   Sorry! There's No Latest Post Yet.
                   </h1>
-                      @endforeach
+                      @endforelse
                      
                     </div>
                     <div class="divider"></div>
