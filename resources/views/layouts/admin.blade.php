570847e178becaf63ef7091cc60424f4715ec9ca
DaffaPrk <Daffa123_>
2022-10-21 07:13:25 +0700

    asd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -599,12 +599,8 @@
         width:400
     });
 </script>
+<!-- datatables -->
 <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript">
-  $(document).ready( function () {
-    $('#table').DataTable();
-} );
-  </script>
   <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 	<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 	<script>
